447583e9307cc0338a231877c6c303e6a0bcf37c
michel bodoldjob <michelscoot@gmail.com>
2026-01-20 10:39:29 +0100

    vider dossier

--- module_http22.js
+++
@@ -1,6 +0,0 @@
-const http = require('http');
-
-http.createServer((request,Response) => {
-    Response.write("premiere requete http avec nodes JS")
-    Response.end()
-}).listen(8080);
--- module_os.js
+++
@@ -1,6 +0,0 @@
-const os = require('os');
-
-console.log("Architecture du CPU : "+os.arch)
-console.log("CPU : "+os.cpus()[0].model)
-console.log("memoire : "+os.totalmem())
-console.log("Dossier utilitaire : "+os.homedir())
